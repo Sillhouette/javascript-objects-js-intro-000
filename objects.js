34462dd6a7ff0987a827df0ac86d39a7cbef08dd
Richard Melchior <raustin.melchior@gmail.com>
2018-05-22 23:28:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,10 +3,9 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign( playlist, {[artistName]: songTitle})
+  return Object.assign( playlist, {[artistName]: songTitle});
 }
 
 function removeFromPlaylist(playlist, artistName){
   delete playlist.artistName;
-  playlist;
 }
