9839e16534617d633fe6bf28f901b3feb48b08e6
Richard Melchior <raustin.melchior@gmail.com>
2018-05-22 23:25:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  return delete playlist.artistName;
-  console.log(playlist)
+  delete playlist.artistName;
+  playlist;
 }
